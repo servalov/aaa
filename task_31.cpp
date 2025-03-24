77f8ec6b27d2a4ab85ea1cb6b4fa73fe272cdbb0
Sergey Valov <sergey.valov.eng@gmail.com>
2025-03-24 22:05:33 +0300

    2

--- task_31.cpp
+++ task_31.cpp
@@ -1,6 +1,30 @@
 //  ООП. Абстракция и инкапсуляция. Задание 1. Адреса
 #include <iostream>
 
+// Объявление класса Address
+class Address
+{
+	private:
+		std::string town{};          // название города
+		std::string street{};        // название улицы
+		std::string home{};          // номер дома
+		std::string flat{};          // номер квартиры
+
+	public:
+	
+		// Конструктор с параметрами
+		Address(std::string setTown, std::string setStreet, std::string setHome, std::string setFlat) : town{ setTown }, street{ setStreet }, home{ setHome }, flat{ setFlat }
+		{};
+		// Конструктор без параметров
+		Address() {};
+
+		// Метод на вывод строки данных
+		std::string get_output_address()
+		{
+			return town + ", " + street + ", " + home + ", " + flat;
+		}
+};
+
 int main()
 {
 	return EXIT_SUCCESS;
